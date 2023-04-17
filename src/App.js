d0f24c3e8fbb06c2ba144f23d4ed32a6d7bca14b
AYHAN GÜNGÖR <gngr.ayhan@gmail.com>
2023-04-17 13:44:23 +0300

    end

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+
 import './App.css';
+
+
+/* COMPONENTS */
+import Header from './components/header/Header';
+import Home from './components/home/Home';
+import About from './components/about/About';
+import Skills from './components/skills/Skills';
+import Services from './components/services/Services';
+import Qualification from './components/qualification/Qualification';
+import Testimonial from './components/testimonial/Testimonial';
+import Contact from './components/contact/Contact';
+import Footer from './components/footer/Footer';
+import Scrollup from './components/scrollup/Scrollup';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <Header/>
+    <main className="main">
+      <Home/>
+      <About/>
+      <Skills/>
+      <Services/>
+      <Qualification/>
+      <Testimonial/>
+      <Contact/>
+    </main>
+      <Footer/>
+      <Scrollup/>
+    </>
+  )
 }
 
 export default App;
---
+++ src/components/skills/Backend.jsx
@@ -0,0 +1,74 @@
+import React from 'react'
+
+
+const Backend = () => {
+  return (
+    <div className="skills__content">
+      <h3 className="skills__title"></h3>
+
+      <div className="skills__box">
+        <div className="skills__group">
+          <div className="skills__data">
+            <i className="bx bx-badge-check"></i>
+
+            <div>
+              <h3 className="skills__name">Node JS</h3>
+              <span className="skills__level">Advanced</span>
+            </div>
+          </div>
+
+          <div className="skills__data">
+            <i className="bx bx-badge-check"></i>
+
+            <div>
+              <h3 className="skills__name">Express JS</h3>
+              <span className="skills__level">Advanced</span>
+            </div>
+          </div>
+
+          <div className="skills__data">
+            <i className="bx bx-badge-check"></i>
+
+            <div>
+              <h3 className="skills__name">MongoDB</h3>
+              <span className="skills__level">Advanced</span>
+            </div>
+          </div>
+          
+        </div>
+
+        <div className="skills__group">
+          <div className="skills__data">
+            <i className="bx bx-badge-check"></i>
+
+            <div>
+              <h3 className="skills__name">ASP MVC</h3>
+              <span className="skills__level">Advanced</span>
+            </div>
+          </div>
+
+          <div className="skills__data">
+            <i className="bx bx-badge-check"></i>
+
+            <div>
+              <h3 className="skills__name">MSSQL</h3>
+              <span className="skills__level">intermediate</span>
+            </div>
+          </div>
+
+          <div className="skills__data">
+            <i className="bx bx-badge-check"></i>
+
+            <div>
+              <h3 className="skills__name">MySql</h3>
+              <span className="skills__level">intermediate</span>
+            </div>
+          </div>
+          
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Backend
